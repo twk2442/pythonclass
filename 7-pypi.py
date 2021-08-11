19e21f5cbd464b753e075a17fb45cde065dc60db
twk2442 <twk2442@gmail.com>
2021-08-11 17:25:35 +0900

    python 입문 마무리,ㅔ뭉ㅁㄴ

--- 7-pypi.py
+++ 7-pypi.py
@@ -1,5 +1,11 @@
 #pypi = python package index  // 패키지  
+# 가상환경 설치후 
 # // 판다스설치 명령어 -- 해당파일 위치로 이동후 python3 -m pip install pandas 
 #// 판다스 기능 알아보기 
-#// 판다스와같은 패키지를 pip로 다운받아서 사용가능(pypi 사이트에서 다운받는것)
-import pandas 
+#// 판다스와같은 패키지를 pip로 다운받아서 사용가능(pypi 사이트에서 다운받는것
+
+import pandas
+
+house =pandas.read_csv('house.csv')
+print(house)
+print(house.describe())
