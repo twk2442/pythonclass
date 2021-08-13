2d55aa1568f1f4270c39298122de3d6fe0d14635
twk2442 <twk2442@gmail.com>
2021-08-13 19:00:45 +0900

    python:dictionary,반복문중첩

---
+++ dic.py
@@ -0,0 +1,13 @@
+#dictionary 
+
+persons = {'name':'egoing','address':'seoul','age':'23'} #dictionary형 
+
+print(persons['name'])  
+print('---------------')
+
+html = [{'name':'tw','address':'illsan','age':'26'},{'name':'ja','address':'seoul','age':'29'},{'name':'sj','address':'seoul','age':'25'}]
+
+for key in html:  #이중반복문을 통해 리스트안에 모든 데이터 출력
+    for data in key:
+        print(data ,':', key[data])     
+    print('--------------------')
